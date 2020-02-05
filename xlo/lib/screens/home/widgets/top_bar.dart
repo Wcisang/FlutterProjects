1dccd2722f5b286568c7bc0145d8b0bee987a6a5
wcisang <william.zischang.cisang@gmail.com>
2020-02-05 10:36:40 -0300

    create xlo project

---
+++ xlo/lib/screens/home/widgets/top_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'bar_button.dart';
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(color: Colors.grey[400],
+          blurRadius: 15.0,
+          offset: Offset(0, 0))
+        ]
+      ),
+      child: Row(
+        children: <Widget>[
+          BarButton(
+            label: "Categorias",
+            boxDecoration: BoxDecoration(color: Colors.white),
+            onPressed: () {},
+          ),
+          BarButton(
+            label: "Filtros",
+            boxDecoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(left: BorderSide(color: Colors.grey[400]))),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
